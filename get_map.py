845b3679936164a43eb712090f5ad8849828f794
Er Ma <erma@placeholder.com>
2024-08-27 20:08:54 +0800

    修改输入输出文件的存放路径；

--- get_map.py
+++ get_map.py
@@ -1,6 +1,9 @@
+import os
 import re
 import csv
 from datetime import datetime
+
+import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
@@ -12,7 +15,7 @@ def get_map_list_from_exg_map_ordering():
     # 写入: ./ExG_ZE_MapList_{timestamp}.csv
 
     # 读取项目根目录下的TXT文件
-    txt_file_path = "maps_html_from_exg_menu.txt"
+    txt_file_path = "input/maps_html_from_exg_menu.txt"
     with open(txt_file_path, "r", encoding="utf-8") as file:
         data = file.read()
 
@@ -23,9 +26,14 @@ def get_map_list_from_exg_map_ordering():
     # 准备写入CSV文件的数据
     csv_data = "\n".join(processed_data)
 
-    # 将数据写入CSV文件，文件名加上时间戳
+    # 确保output目录存在，如果不存在则创建它
+    output_dir = 'output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
+    # 将数据写入CSV文件，文件名加上时间戳，并确保输出到output目录
     timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-    csv_file_path = f"ExG_ZE_MapList_{timestamp}.csv"
+    csv_file_path = os.path.join(output_dir, f"ExG_ZE_MapList_{timestamp}.csv")
     with open(csv_file_path, "w", encoding="utf-8-sig") as file:
         file.write(csv_data)
 
@@ -70,10 +78,17 @@ def get_map_list_from_exg_map_list():
         print("没有找到任何数据")
         return
 
+    # 确保output目录存在，如果不存在则创建它
+    output_dir = 'output'
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+
     # 写入CSV文件
     timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
     filename = f'ExG_MapList_{timestamp}.csv'
-    with open(filename, 'w', newline='', encoding='utf-8-sig') as csvfile:
+    csv_file_path = os.path.join(output_dir, filename)
+    with open(csv_file_path, 'w', newline='', encoding='utf-8-sig') as csvfile:
         writer = csv.writer(csvfile)
         writer.writerow(['地图名', '中文名', '难度', '冷却时长', '冷却截止', 'Workshop'])
         writer.writerows(data)
+
