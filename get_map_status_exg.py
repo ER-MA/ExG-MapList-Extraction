9e48cd5d84dffdb4626d2e318717f3ca3c8de601
Er Ma <erma@placeholder.com>
2024-08-27 22:02:22 +0800

    添加异常处理；

--- get_map_status_exg.py
+++ get_map_status_exg.py
@@ -8,25 +8,30 @@ def get_current_exg_map_status():
     headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
     }
-    request_url = "https://list.darkrp.cn:9000/ServerList/CurrentCs2MapStatus"  # 当前ExG地图状态文档的请求地址
-    map_status_doc = requests.get(request_url, headers=headers)  # Get文档并暂存
-    return map_status_doc.json()
+    request_url = "https://list.darkrp.cn:9000/ServerList/CurrentCs2MapStatus"
+    try:
+        with requests.get(request_url, headers=headers) as response:
+            response.raise_for_status()  # 如果请求失败，将抛出HTTPError异常
+            return response.json()
+    except requests.RequestException as e:
+        print(f"请求发生错误: {e}")
+        return None
 
 
 # 将Difficulty由数值转换为文字
-def get_difficulty_name(n):
+def get_difficulty_name(difficulty_level):
     difficulties = ["简单", "普通", "困难", "极难", "史诗", "梦魇", "绝境"]
-    return difficulties[n] if 0 <= n < len(difficulties) else n
+    return difficulties[difficulty_level] if 0 <= difficulty_level < len(difficulties) else "未知"
 
 
 # 格式化CooldownMinute（与官网方式处理相同）
-def format_minute(n):
-    if n < 180:
-        return f"{n:.0f}分"
-    elif n < 4320:
-        return f"{n / 60:.1f}时"
+def format_minute(minutes):
+    if minutes < 180:
+        return f"{minutes:.0f}分"
+    elif minutes < 4320:
+        return f"{minutes / 60:.1f}时"
     else:
-        return f"{n / 1440:.1f}天"
+        return f"{minutes / 1440:.1f}天"
 
 
 # 对原始的CurrentCs2MapStatus数据进行预处理，转为可阅读的json格式数据
@@ -49,5 +54,5 @@ def preprocess_data(json_data):
         }
         processed_data.append(processed_info)
 
-    return json.dumps(processed_data, ensure_ascii=False)
+    return json.dumps(processed_data, ensure_ascii=False, indent=4)
 
